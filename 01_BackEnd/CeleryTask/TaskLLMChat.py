7e2e4770a0fb902bb2aa5b83a4ea00de9c3615da
vansar <vansar.w@gmail.com>
2025-04-16 00:04:27 +0800

    完整修订

--- 01_BackEnd/CeleryTask/TaskLLMChat.py
+++ 01_BackEnd/CeleryTask/TaskLLMChat.py
@@ -4,6 +4,4 @@ from . import CELERY_APP
 @CELERY_APP.task
 def Task_ChatResponse(prompt):
     response = ChatResponse(prompt)
-    #LMStudioResponse的数据类型为 <class 'lmstudio.json_api.PredictionResult'>
-    #因此需要转换
     return response
