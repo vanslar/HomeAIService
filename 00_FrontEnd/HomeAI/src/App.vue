eb941285fb06c8cfa0347c72dedd827d6de71372
vansar <vansar.w@gmail.com>
2025-04-16 23:52:15 +0800

    尝试布局w

--- 00_FrontEnd/HomeAI/src/App.vue
+++ 00_FrontEnd/HomeAI/src/App.vue
@@ -1,17 +1,20 @@
 <script setup>
+import NavView from './Views/NavView.vue';
 </script>
 
 <template>
   <div class="common-layout">
     <el-container>
-        <el-aside width="200px">Aside</el-aside>
+        <el-aside width="200px">
+            <NavView />
+        </el-aside>
         <el-container>
             <el-header>
-                Header
-                commasn
+              header
             </el-header>
             <el-main>
-                Main
+              <div id="main" style="width: 600px;height:400px">main</div>
+              main
             </el-main>
             <el-footer>
                 Footer
