8ee2f6f288197e4ee8ec3f195aa86ad76ed082c4
vansar <vansar@vansar.com>
2025-04-18 17:27:07 +0800

    创建测试模版页

--- 00_FrontEnd/HomeAI/src/Views/HomeView/HomeView_test.vue
+++ 00_FrontEnd/HomeAI/src/Views/HomeView/HomeView_test.vue
@@ -1,9 +1,17 @@
 <script setup>
+    import LineChart from '../../components/ChartView/LineChart.vue'
+    const funcDataLoader = () => {
+        return {
+            x: [1, 2, 3, 4, 5],
+            y: [7, 4, 3, 1, 5]
+        }
+    }
 </script>
 
 <template>
     <div>
         <h1>Test</h1>
+        <LineChart :funcDataLoader="funcDataLoader" />
     </div>
 </template>
 
---
+++ 00_FrontEnd/HomeAI/src/components/ChartView/LineChart.vue
@@ -0,0 +1,23 @@
+<script setup>
+    import { ref, onMounted, defineProps } from 'vue'
+    const props = defineProps({
+        funcDataLoader: {
+            type: Function,
+            required: false 
+        }
+    });
+    var data = ref(0)
+
+    onMounted(() => {
+        console.log("LineChart")
+        console.log(props.funcDataLoader())
+        data.value = props.funcDataLoader()
+
+    })
+</script>
+<template>
+    <h1>LineChart</h1>
+    <h3>{{ data.x }}</h3>
+    <h3>{{ data.y }}</h3>
+</template>
+<style scoped></style>
