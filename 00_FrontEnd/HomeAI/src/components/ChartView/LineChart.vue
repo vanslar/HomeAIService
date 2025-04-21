1bbe61b78b07e38ae82fea54038286fe3e571457
vansar <vansar@vansar.com>
2025-04-21 14:55:18 +0800

    init 完善lineChar的view界面

--- 00_FrontEnd/HomeAI/src/components/ChartView/LineChart.vue
+++ 00_FrontEnd/HomeAI/src/components/ChartView/LineChart.vue
@@ -1,5 +1,6 @@
 <script setup>
-    import { ref, onMounted, defineProps } from 'vue'
+    import { reactive, ref, onMounted, defineProps } from 'vue'
+    import * as echarts from 'echarts';
     const props = defineProps({
         funcDataLoader: {
             type: Function,
@@ -7,17 +8,22 @@
         }
     });
     var data = ref(0)
+    var lineChart_Data = reactive({
+        x: [1, 2, 3, 4, 5],
+        y: [7, 4, 3, 1, 5]
+    })
 
     onMounted(() => {
         console.log("LineChart")
         console.log(props.funcDataLoader())
         data.value = props.funcDataLoader()
 
+        var dom = document.getElementById('main')
+        var myChart = echarts.init(dom);
+
     })
 </script>
 <template>
-    <h1>LineChart</h1>
-    <h3>{{ data.x }}</h3>
-    <h3>{{ data.y }}</h3>
+    <div id="main" style="width: 100%;height:100%;"></div>
 </template>
 <style scoped></style>
